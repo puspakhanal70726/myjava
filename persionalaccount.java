96779df13dbbab64f1a5b2e1d3f2a1237393e754
Puspa Khanal <31549620+puspakhanal70726@users.noreply.github.com>
2020-03-09 14:26:34 -0700

    Update and rename Computer_Bank_of_India.java to persionalaccount.java

--- persionalaccount.java
+++ persionalaccount.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.Random;
-public class Computer_Bank_of_India {
+public class persionalaccount {
 public static int NewRandom(int min, int max) {
     Random rand = new Random();
     int randomNum = rand.nextInt((max - min) + 1) + min;
